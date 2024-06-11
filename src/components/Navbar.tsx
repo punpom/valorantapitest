2e620b1516e6acf2c56a304d4d74b40f74683b95
Punpom <carrilhogomeslucas@gmail.com>
2024-06-12 00:02:08 +0200

    navbar start

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar: React.FC = ({}) => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>Agents</li>
+                <li>Maps</li>
+                <li>Weapons</li>
+                <li>Bundles</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
