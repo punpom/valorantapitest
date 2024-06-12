8ca7deb90d3dd5c839fcf488bc5e8aff6fc22139
Punpom <carrilhogomeslucas@gmail.com>
2024-06-12 22:04:08 +0200

    service details

--- src/services/agentsServices.ts
+++ src/services/agentsServices.ts
@@ -1,6 +1,26 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { AgentType } from "../types/types";
 
-export const fetchAgents = async () => {
-    const response = await axios.get('https://valorant-api.com/v1/agents?isPlayableCharacter=true');
-    return response.data;
+//API constant
+const API_URL = 'https://valorant-api.com/v1/agents?isPlayableCharacter=true'
+
+interface AgentsResponse {
+    status: number;
+    data: AgentType[];
+}
+
+export const fetchAgents = async (): Promise<AgentType[]> => {
+    try {
+    
+    const response: AxiosResponse<AgentsResponse> = await axios.get(API_URL);
+
+    // returning the data received
+    return response.data.data;
+
+    } catch (error) {
+
+        console.error(error);
+
+        throw error;
+    }
 }
